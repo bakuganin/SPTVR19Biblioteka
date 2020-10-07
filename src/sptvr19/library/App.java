beb943714cdc404dfb8a2302e926a5c1ee42d386
Bakugan <kafder97@gmail.com>
2020-10-07 09:33:45 +0300

    Можно добавлять читателей и книги

--- src/sptvr19/library/App.java
+++ src/sptvr19/library/App.java
@@ -14,8 +14,11 @@ import java.util.Scanner;
  * @author user
  */
 class App {
-    private Book[] books = new Book[10];
+    private Book[] books = new Book[2];
+    private Read[] readers = new Read[10];
     public void run() {
+        int amountBook = -1;
+        int amountReaders = -1;
         boolean repeat = true;
         System.out.println("<--- Библиотека --->");
         do {
@@ -37,22 +40,38 @@ class App {
                     break;
                 case "1":
                     System.out.println("<--- Добавить новую книгу --->");
-                    Book book = new Book("War and Peace", "L.Tolstoy", 2010);
-                    System.out.println("Добавлена книга: " + book.getName());
-                    books[0] = book;
+                    amountBook = amountBook + 1;
+                    if (amountBook != 2 || amountBook < 2){
+                        System.out.print("Введите название кники: ");
+                        String bookName = input.nextLine();
+                        System.out.print("Введите Автора: ");
+                        String bookAuthor = input.nextLine();
+                        System.out.print("Введите год издание книги: ");
+                        Integer bookYear = input.nextInt();
+                        Book book = new Book(bookName, bookAuthor, bookYear);
+                        System.out.println("Добавлена книга: " + book.getName());
+                        books[amountBook] = book;
+                    }
+                    else{
+                        System.out.println("В библиотеке закончилось место.");
+                    }
                     break;
 
                 case "2":
                     System.out.println("<--- Cписок книг --->");
                     for (int i = 0; i < books.length; i++){
                         if(books[i] != null){
-                            System.out.println(books[i].getName());
+                            System.out.printf("%3d. Название книги: %s%n     Автор: %s%n"
+                                    ,i+1
+                                    ,books[i].getName()
+                                    ,books[i].getAuthor());
                         }
                     }
                     break;
 
                 case "3":
                     System.out.println("<--- Зарегестрировать пользователя --->");
+                    amountReaders += 1;
                     System.out.print("Введите имя: ");
                     String name = input.nextLine();
                     System.out.print("Введите фамилию: ");
@@ -61,10 +80,16 @@ class App {
                     String phone = input.nextLine();
                     Read read = new Read(name, surname, phone);
                     System.out.println("Добавлен пользователь: " + read.getName() + " " + read.getSurname());
+                    readers[amountReaders] = read;
                     break;
                 
                 case "4":
                     System.out.println("<--- Список пользователей --->");
+                    for (int i = 0; i < readers.length; i++){
+                        if(readers[i] != null){
+                            System.out.println(readers[i].getName());
+                        }
+                    }
                     break;
                     
                 case "5":
